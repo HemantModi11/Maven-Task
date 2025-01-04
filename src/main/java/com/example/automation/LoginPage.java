c347bd136daad2d9d69ab36674b97d571329f817
Hemant Modi <90129538+HemantModi11@users.noreply.github.com>
2025-01-04 13:22:38 +0530

    Update LoginPage.java

--- src/main/java/com/example/automation/LoginPage.java
+++ src/main/java/com/example/automation/LoginPage.java
@@ -3,37 +3,49 @@ package com.example.automation;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
-import org.openqa.selenium.chrome.ChromeDriver;
-import org.junit.jupiter.api.Test;
-import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class LoginPage {
+    private final WebDriver driver;
 
-    @Test
-    public void testLogin() {
-        // Set up the WebDriver
-        System.setProperty("webdriver.chrome.driver", "C:\\tools\\chromedriver-win64\\chromedriver.exe");
-        WebDriver driver = new ChromeDriver();
+   
+    public LoginPage(WebDriver driver) {
+        this.driver = driver;
+    }
 
-        try {
-            // Navigate to the login page
-            driver.get("https://practicetestautomation.com/practice-test-login/");
+   
+    public void openLoginPage(String url) {
+        driver.get(url);
+    }
 
-            WebElement usernameField = driver.findElement(By.id("username"));
-            WebElement passwordField = driver.findElement(By.id("password"));
-            WebElement loginButton = driver.findElement(By.id("submit"));
+   
+    public void setUsername(String username) {
+        WebElement usernameField = driver.findElement(By.id("username"));
+        usernameField.sendKeys(username);
+    }
 
+    
+    public void setPassword(String password) {
+        WebElement passwordField = driver.findElement(By.id("password"));
+        passwordField.sendKeys(password);
+    }
 
-            usernameField.sendKeys("student");
-            passwordField.sendKeys("Password123");
-            loginButton.click();
+    
+    public void clickLoginButton() {
+        WebElement loginButton = driver.findElement(By.cssSelector("button[type='submit']"));
+        loginButton.click();
+    }
 
-            String expectedTitle = "Logged In Successfully | Practice Test Automation";
-            String actualTitle = driver.getTitle();
-            assertEquals(expectedTitle, actualTitle);
+    
+    public String getSuccessMessage() {
+        WebElement messageElement = driver.findElement(By.id("flash"));
+        return messageElement.getText();
+    }
 
-        } finally {
-            driver.quit();
-        }
+    
+
+    
+    public String getErrorMessage() {
+        WebElement errorElement = driver.findElement(By.id("error"));
+        return errorElement.getText();
     }
 }
